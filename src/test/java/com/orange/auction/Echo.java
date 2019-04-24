8bb1a32b7663f253fbdad65dfdcc910d1e45d78e
Suraj <suraj.kumar@care.com>
2019-04-24 19:54:53 +0530

    fix web layer integrtion test

--- src/test/java/com/orange/auction/Echo.java
+++ src/test/java/com/orange/auction/Echo.java
@@ -1,26 +1,60 @@
 package com.orange.auction;
 
-import com.jayway.restassured.RestAssured;
-import com.jayway.restassured.response.ResponseBody;
-import org.junit.Assert;
+import com.orange.auction.endpoints.SampleEndpoint;
+import com.orange.auction.model.Member;
+import com.orange.auction.service.MemberService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 
-import static com.jayway.restassured.RestAssured.get;
-import static org.hamcrest.Matchers.*;
+import java.util.ArrayList;
+import java.util.List;
 
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(classes = AuctionApplication.class)
-//@TestPropertySource(value={"classpath:application.properties"})
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(SampleEndpoint.class)
 public class Echo {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private MemberService memberService;
+
     @Test
-    public void echo(){
-        Assert.assertEquals("Should get an OK response", 200, RestAssured.given()
-        .when().get("echo/{greeting}", "hello").getStatusCode());
+    public void echo() throws Exception {
+        mockMvc.perform(get("/echo/hello")).andExpect(status().isOk());
+    }
+
+    @Test
+    public void mockedMember() throws Exception {
+        given(this.memberService.getUser("suraj")).willReturn(new Member("Suraj"));
+        mockMvc.perform(get("/users/suraj")).andExpect(status().isOk());
+    }
+
+    @Test
+    public void numberOfMembers() throws Exception {
+        List<Member> members = new ArrayList<>();
+        members.add(new Member("Suraj"));
+        members.add(new Member("sujit"));
+
+        given(this.memberService.getUsers()).willReturn(members);
+        mockMvc.perform(get("/users")).andExpect(status().isOk());
     }
 }
+
+/*If it is required to test only the web layer, i.e if the controllers are working fine or not, then
+* @WebMvcTest should be the way to go.*/
